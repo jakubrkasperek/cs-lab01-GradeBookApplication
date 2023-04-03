021355c4065596138e8d3f1e296d67d21c4d401a
jakubrkasperek <jakub.r.kasperek@gmail.com>
2023-04-03 20:40:32 +0200

    Update

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace GradeBook.GradeBooks
+namespace GradeBook.GradeBooks 
 {
     public class RankedGradeBook : BaseGradeBook
     {
--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    internal class Class1
+    public class StandardGradeBook : BaseGradeBook
     {
+        public StandardGradeBook(string name) : base(name)
+        {
+
+        }
     }
 }
